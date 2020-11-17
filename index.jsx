fb46e9cb117cf7f3202a8d10c17d36ea901eb55a
Nathally Souza <nathallyccd@gmail.com>
2020-11-17 09:50:35 -0300

    Implementando React e admin

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './src/components/Index';
+
+ReactDOM.render(
+  <Index/>,
+  document.getElementById('root')
+);
---
+++ src/components/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <>
+        
+        <main>Olá Mundo com React;</main>
+           
+    </>
+  );
+}
+
+export default Index;
